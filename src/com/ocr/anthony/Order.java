8a2ae77700eab61b611f9520652b552a009b1086
fthos <fredthos78@gmail.com>
2021-03-29 16:55:03 +0200

    Add: - method displayAvailableMenu - method displaySelectionMenu

--- src/com/ocr/anthony/Order.java
+++ src/com/ocr/anthony/Order.java
@@ -5,6 +5,11 @@ public class Order {
      * Display all available menus in the restaurant.
      */
     public void displayAvailableMenu() {
+        System.out.println("choix du menu");
+        System.out.println("1 - Poulet");
+        System.out.println("2 - Boeuf");
+        System.out.println("3 - Vegetarien");
+        System.out.println("Que souhaitez-vous comme Menu");
 
     }
     /**
@@ -12,6 +17,6 @@ public class Order {
      * @param nbMenu The selected menu.
      */
     public void displaySelectedMenu(int nbMenu) {
-
+        System.out.println("Vous avez choisi le menu"+ nbMenu);
     }
 }
